508c6e11e122a91361d0f6cd7ce9c42e62b3af10
ranasidhu91-debug <ranaatinder@outlook.com>
2023-02-04 21:17:47 +0800

    MOdifications

--- Week 2/GPT_Exercise2.c
+++ Week 2/GPT_Exercise2.c
@@ -10,38 +10,36 @@
 int shared_variable = 0;
 pthread_mutex_t g_Mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *increment(void *arg)
+void *increment_variable(void *arg)
 {
-    int *p = (int*)arg;
+    int *p = (int*) arg;
     int number = *p;
     for (int i = 0; i < number; i++)
     {
         pthread_mutex_lock(&g_Mutex);
         shared_variable++;
         pthread_mutex_unlock(&g_Mutex);
-
     }
     return NULL;
 }
 
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
-
-    int threads_number = atoi(argv[1]);
-    printf("%d\n",threads_number);
-    pthread_t threads[threads_number];
-    int increments[threads_number];
+    int threadNumber = atoi(argv[1]);
+    pthread_t threads[threadNumber];
+    int increments[threadNumber];
     pthread_mutex_init(&g_Mutex,NULL);
-    for (int i = 0; i < threads_number; i++)
+    for (int i = 0; i < threadNumber; i++)
     {
         increments[i] = 100;
-        pthread_create(&threads[i],NULL,&increment,&increments[i]);
+        pthread_create(&threads[i],NULL,&increment_variable,&increments[i]);
     }
-    for (int i = 0; i < threads_number; i++)
+    for (int i = 0; i < threadNumber; i++)
     {
         pthread_join(threads[i],NULL);
     }
+
     pthread_mutex_destroy(&g_Mutex);
-    printf("FInal value: %d\n",shared_variable);
+    printf("The final value is %d\n",shared_variable);
     return 0;
 }
